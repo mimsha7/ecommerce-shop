5697ece179a030a92864044f0e9c878540a0f58d
Md.Mimshadur Rahman <rahman15-6119@diu.edu.bd>
2025-09-08 01:40:01 +0600

    create integration test for product component

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -28,7 +28,7 @@ export function Product({ product, addCart }) {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image" data-testid="product-image" src={product.image} />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
@@ -36,6 +36,7 @@ export function Product({ product, addCart }) {
       <div className="product-rating-container">
         <img
           className="product-rating-stars"
+          data-testid="product-rating-stars-image"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
         <div className="product-rating-count link-primary">
